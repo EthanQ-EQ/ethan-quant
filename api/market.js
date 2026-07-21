9cb7e696c595fbc96b82437f0e95587395c7a4ed
EthanQ-EQ <zhih3561@gmail.com>
2026-07-21 11:55:02 +0800

    市场温度

--- api/market.js
+++ api/market.js
@@ -2,31 +2,19 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   try {
-    // 上证、深证、创业板、成交额
+    // 上证、深证、创业板
     const urls = [
       "https://push2.eastmoney.com/api/qt/stock/get?secid=1.000001&fields=f43,f170,f58",
       "https://push2.eastmoney.com/api/qt/stock/get?secid=0.399001&fields=f43,f170,f58",
-      "https://push2.eastmoney.com/api/qt/stock/get?secid=0.399006&fields=f43,f170,f58",
-      "https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&invt=2&fields=f137"
+      "https://push2.eastmoney.com/api/qt/stock/get?secid=0.399006&fields=f43,f170,f58"
     ];
 
-    const [shRes, szRes, cybRes, amountRes] =
+    const [shRes, szRes, cybRes] =
       await Promise.all(urls.map(url => fetch(url)));
 
     const sh = (await shRes.json()).data;
     const sz = (await szRes.json()).data;
     const cyb = (await cybRes.json()).data;
-
-    const amountJson = await amountRes.json();
-
-    let totalAmount = 0;
-
-    if (amountJson.data?.diff) {
-      totalAmount = amountJson.data.diff.reduce(
-        (sum, item) => sum + (item.f137 || 0),
-        0
-      );
-    }
 
     // 根据涨跌幅评分（0~20）
     function scoreIndex(percent) {
@@ -50,22 +38,8 @@ export default async function handler(req, res) {
       ) / 2
     );
 
-    // 上涨家数（下一版接入真实数据）
-    const advanceScore = 10;
-
-    // 两市成交额评分
-    const amountYi = totalAmount / 100000000;
-
-    let amountScore = 0;
-
-    if (amountYi >= 25000) amountScore = 15;
-    else if (amountYi >= 20000) amountScore = 13;
-    else if (amountYi >= 15000) amountScore = 11;
-    else if (amountYi >= 12000) amountScore = 9;
-    else if (amountYi >= 9000) amountScore = 7;
-    else amountScore = 5;
-
     // 下一版接入真实数据
+    const advanceScore = 10;
     const fundScore = 8;
     const emotionScore = 8;
 
@@ -73,7 +47,6 @@ export default async function handler(req, res) {
       shScore +
       szScore +
       advanceScore +
-      amountScore +
       fundScore +
       emotionScore;
 
@@ -94,7 +67,6 @@ export default async function handler(req, res) {
         shanghai: shScore,
         sz: szScore,
         advance: advanceScore,
-        amount: amountScore,
         fund: fundScore,
         emotion: emotionScore
       },
@@ -114,9 +86,8 @@ export default async function handler(req, res) {
           name: cyb.f58,
           changePercent: cyb.f170 / 100
         }
-      },
+      }
 
-      amount: Math.round(amountYi)
     });
 
   } catch (err) {
